a04d2b6ef8bcff12f86778b793521cf3de34fab0
RoyalRoader <sksksdlqk@naver.com>
2024-05-23 16:59:55 +0900

    .

--- consumer/src/main/java/consumer/Controller.java
+++ consumer/src/main/java/consumer/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController 
-public class Controller {
+public class Controller { 
 	
 	@GetMapping("/gethc")
 	public Mono<ResponseEntity<String>> gealthCheck() throws Exception {
